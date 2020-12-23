67050c7b30bd37973ae451934c835f6c518d4cf5
lixiaolong20190101 <2718893665@qq.com>
2020-12-23 20:59:11 +0800

    开发人员提交

--- src/com/yidu/TestCase.java
+++ src/com/yidu/TestCase.java
@@ -3,11 +3,12 @@ package com.yidu;
 /**
  * @description:
  * @author:lishaolong
- * @dateTime:2020/12/23 14:06
+ * @dateTime:2020/12/23
  * @version:1.0
  */
 public class TestCase {
     public static void main(String[] args) {
-        System.out.println("一度教育 ");
+
+        System.out.println("一度教育 努力做得更好！");
     }
 }
