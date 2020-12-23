b1885373f8f589b68626959664145cebb780f436
lixiaolong20190101 <2718893665@qq.com>
2020-12-23 14:08:45 +0800

    git测试第一次提交

---
+++ src/com/yidu/TestCase.java
@@ -0,0 +1,13 @@
+package com.yidu;
+
+/**
+ * @description:
+ * @author:lishaolong
+ * @dateTime:2020/12/23 14:06
+ * @version:1.0
+ */
+public class TestCase {
+    public static void main(String[] args) {
+        System.out.println("一度教育 努力做更好！");
+    }
+}
